07832afc4e8c29b90c99e897aa049e5fefe6cb71
maxmat2501 <blouin.maxence.pro@gmail.com>
2020-01-23 14:28:25 +0100

    Phase 2 Start button

--- Assets/Scripts/AI_Control.cs
+++ Assets/Scripts/AI_Control.cs
@@ -12,41 +12,50 @@ public class AI_Control : MonoBehaviour
     [SerializeField] private GameObject m_Target2;
     public bool enter = true;
     public bool exit = true;
+    int Phase2 = 0;
 
+    /* void OnGUI()
+     {
+         Event e = Event.current;
+         if (e.button == 0 && e.isMouse)
+         {
+             Debug.Log("Left Click");
+         }
+         else if (e.button == 1)
+         {
+             Debug.Log("Right Click");
+         }
+         else if (e.button == 2)
+         {
+             Debug.Log("Middle Click");
+         }
+     }*/
 
-   /* void OnGUI()
+    // Start is called before the first frame update
+    void Start()
     {
-        Event e = Event.current;
-        if (e.button == 0 && e.isMouse)
-        {
-            Debug.Log("Left Click");
-        }
-        else if (e.button == 1)
-        {
-            Debug.Log("Right Click");
-        }
-        else if (e.button == 2)
-        {
-            Debug.Log("Middle Click");
-        }
-    }*/
-
-        // Start is called before the first frame update
-        void Start()
-    {
-        m_NavMeshAgent.SetDestination(m_Target.transform.position);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-       
+        if (Input.GetButtonDown("Jump") && Phase2 == 0)
+        {
+            StartPhase2();
+            Phase2++;
+        }
+
     }
     private void OnTriggerEnter(Collider other)
     {
         m_NavMeshAgent.SetDestination(m_Target2.transform.position);
         
     }
- 
+    private void StartPhase2()
+    {
+        m_NavMeshAgent.SetDestination(m_Target.transform.position);
+
+    }
+
 }
