692f9b6298d7bf76fffbc65514e53313f6ee1a28
Bodinio <albanbodet@gmail.com>
2020-05-05 13:04:13 +0200

    Ajouts 3D_Characters + Animations Faciales

---
+++ Assets/Scripts/Anim_Transition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim_Transition : MonoBehaviour
+
+{
+
+    public Animation anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        /* void Update()
+         {
+             // Créer variable "PEUR" dans la jauge pour pouvoir trigger l'animation suivante.
+             if (int PEUR >= 50)
+         {
+             anim.SetBool("Is_Stressed", true);
+         }
+         else
+         {
+             anim.SetBool("Is_Stressed =false", false);
+         }
+
+     }
+             */
+    }
+}
