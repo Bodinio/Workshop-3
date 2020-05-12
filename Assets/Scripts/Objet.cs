5863c3ad07ae4e8870864ebc224e1c4e3e12e181
Ascanyell <jaimisunjhin@gmail.com>
2020-05-12 09:50:19 +0100

    ajout script objet

---
+++ Assets/Scripts/Objet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "nouvel objet", menuName = "Objet" )]
+public class Objet : ScriptableObject
+{
+    public string objName;
+    public string description;
+
+    public Sprite model3d;
+
+    public int range;
+    public int pointsFear;
+    public int slow;
+
+    public bool champVision;
+    public bool meuble;
+    public bool lumière;
+    public bool sonore;
+
+
+}
