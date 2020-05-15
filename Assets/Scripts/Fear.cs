4110cf231729ccdc96b3cf26e1a98e2c8b7745c3
Ascanyell <jaimisunjhin@gmail.com>
2020-05-15 08:54:33 +0100

    ajouts scripts comportement objets

---
+++ Assets/Scripts/Fear.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Fear : MonoBehaviour
+{
+    public float timeFear;
+    public Transform target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private IEnumerator FearTarget()
+    {
+        agent.speed = 0.15f;
+        yield return new WaitForSeconds(timeFear);
+        agent.speed = 0.75f;
+    }
+}
---
+++ Assets/Scripts/Slow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Slow : MonoBehaviour
+{
+    public float timeSlow;
+    public Transform target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private IEnumerator SlowTarget()
+    {
+        agent.speed = 0.25f;
+        yield return new WaitForSeconds(timeSlow);
+        agent.speed = 0.75f;
+    }
+}
