af8dbef4e75659607feb74f1eefe9438a4871d5f
ShapapK <binz.elisa@gmail.com>
2020-05-27 11:51:59 +0200

    PUTAIN D'ANIMATION DE MERDE

---
+++ Assets/Scripts/Cinematique.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cinematique : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+
+    void Update()
+    {
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Vous avez cliqué");
+                anim.SetBool("Transition", true);
+            }
+
+        }
+
+    }
+}
