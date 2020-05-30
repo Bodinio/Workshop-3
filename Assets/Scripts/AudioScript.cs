4bb15e7c86d4121e7a3f20e414cc0104cf833607
Bodinio <albanbodet@gmail.com>
2020-05-31 00:40:37 +0200

    Prefabs Sons et  Animation + Profond Désespoir

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    private MusicClass _audiosource;
+    public AudioSource scene1;
+    // Use this for initialization
+    void Start()
+    {
+        scene1 = GameObject.Find("AudioSource").GetComponent<AudioSource>();
+        Debug.Log("TEST");
+    }
+
+    public void Stop()
+    {
+        scene1.Stop();
+    }
+}
--- Assets/Scripts/MusicClass.cs
+++ Assets/Scripts/MusicClass.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicClass : MonoBehaviour
 {
     private AudioSource _audioSource;
+    private AudioSource _musicLevel;
+
     private void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
@@ -13,6 +16,12 @@ public class MusicClass : MonoBehaviour
     {
         if (_audioSource.isPlaying) return;
         _audioSource.Play();
+
+       /* if (SceneManager.GetActiveScene().name == "1-1" || SceneManager.GetActiveScene().name == "1-2" || SceneManager.GetActiveScene().name == "1-3" || SceneManager.GetActiveScene().name == "1-4")
+        {
+            _audioSource.Stop();
+            _musicLevel.Play();
+        }*/
     }
 
     public void StopMusic()
